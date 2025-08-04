472fe876d2fc2dd6032654dca98b2aa9d1cb603a
t-poirier <tri.poirier@basikon.io>
2025-08-04 11:48:19 +0200

    Header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row">
     <div class="col-xs-12">
-      <div class="max-w-5xl m-auto flex justify-between">
+      <div class="max-w-5xl m-auto flex justify-between items-center h-[44px]">
         <NuxtLink
-          class="mr-5 h-[30px] w-[200px] bg-no-repeat bg-center bg-contain z-[1]"
+          class="mr-5 h-[22px] w-[200px] bg-no-repeat bg-center bg-contain"
           :style="{
             'background-image': `url(${resourcesUrl}/img/customers/basikon.svg)`,
           }"
@@ -18,11 +18,12 @@
           <div class="ml-5 mr-5">{{ $t("resources") }}</div>
         </div>
 
+        <div class="ml w-[200px]">
         <select @change="switchLocale($event)" name="locales">
           <option v-for="locale in availableLocales" :key="locale.code" :value="locale.code" :selected="locale.code === currentLocale">
             {{ locale.name }}
           </option>
-        </select>
+        </select></div>
       </div>
     </div>
   </div>
