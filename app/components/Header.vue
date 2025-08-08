7d16a5389fd9d956dadb8066be4b33f3ee7dffa7
t-poirier <tri.poirier@basikon.io>
2025-08-08 17:08:26 +0200

    Header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row">
     <div class="col-xs-12">
-      <div class="max-w-5xl m-auto flex justify-between items-center h-[44px]">
+      <div class="max-w-[2560px] m-auto flex justify-between items-center h-[44px]">
         <NuxtLink
-          class="mr-5 h-[22px] w-[200px] bg-no-repeat bg-center bg-contain"
+          class="mr-5 h-[22px] w-[270px] bg-no-repeat bg-center bg-contain"
           :style="{
             'background-image': `url(${resourcesUrl}/img/customers/basikon.svg)`,
           }"
@@ -12,18 +12,24 @@
         </NuxtLink>
 
         <div class="hidden sm:flex">
-          <div class="ml-5 mr-5">{{ $t("solutions") }}</div>
-          <div class="ml-5 mr-5">{{ $t("customers") }}</div>
+          <div class="ml-5 mr-5">{{ $t("solution") }}</div>
+          <div class="ml-5 mr-5">{{ $t("ourClients") }}</div>
           <NuxtLink class="ml-5 mr-5" :to="localePath('/about')">{{ $t("aboutUs") }}</NuxtLink>
-          <div class="ml-5 mr-5">{{ $t("resources") }}</div>
+          <div class="ml-5 mr-5">{{ $t("newsroom") }}</div>
+          <div class="ml-5 mr-5">{{ $t("aiHub") }}</div>
         </div>
 
-        <div class="ml w-[200px]">
-        <select @change="switchLocale($event)" name="locales">
-          <option v-for="locale in availableLocales" :key="locale.code" :value="locale.code" :selected="locale.code === currentLocale">
-            {{ locale.name }}
-          </option>
-        </select></div>
+        <div class="ml w-[270px] flex">
+          <a href="/demo-request" class="mr-2 ml-2 pointer-events-auto button button-sm">
+            {{ $t("requestDemo") }}
+          </a>
+
+          <select @change="switchLocale($event)" name="locales">
+            <option v-for="locale in availableLocales" :key="locale.code" :value="locale.code" :selected="locale.code === currentLocale">
+              {{ locale.code.toUpperCase() }}
+            </option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
