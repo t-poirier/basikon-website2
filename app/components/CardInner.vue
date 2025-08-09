58cab896cd49b2228dc039d7c9b22830f101bd18
t-poirier <tri.poirier@basikon.io>
2025-08-09 15:13:58 +0200

    w

--- app/components/CardInner.vue
+++ app/components/CardInner.vue
@@ -1,31 +1,28 @@
 <template>
   <div :class="colClassName + (inArray ? '' : ' !p-0')">
-    <div :class="'mb-4 relative overflow-hidden' + (height ? ` h-${height} min-h-${height}` : '')">
+    <div :class="'mb-4 relative overflow-hidden flex items-center justify-center' + (height ? ` h-${height} min-h-${height}` : '')">
       <NuxtLink v-if="bgHref" :href="localePath(bgHref)" class="w-full h-full z-[3] left-[0] top-[0] absolute" tabindex="-1"></NuxtLink>
 
+      <video v-if="background?.url && background?.type === 'video'" autoplay muted loop playsinline class="absolute w-full h-full z-[1]">
+        <source :src="background.url" type="video/mp4" />
+      </video>
       <div
-        v-if="bgImg?.url || bgCss"
+        v-else-if="(background?.url || background?.color) && ['image', undefined].includes(background?.type)"
         class="absolute w-full h-full bg-no-repeat bg-center bg-cover z-[1]"
         :style="{
           ...{
             // bg image can also be provided in bgCss
-            background: bgCss,
-            'background-image': bgImg?.url ? `url(${resourcesUrl}${bgImg.url})` : undefined,
-            'background-position': bgImg?.position,
-            'background-size': bgImg?.size,
+            'background-color': background.color,
+            'background-image': background.url ? `url(${resourcesUrl}${background.url})` : undefined,
+            'background-position': background.position,
+            'background-size': background.size,
           },
         }"
       ></div>
-      <video v-else-if="bgVideo?.url" autoplay muted loop playsinline class="absolute w-full h-full z-[1]">
-        <source :src="bgVideo.url" type="video/mp4" />
-      </video>
 
       <ImgSlider v-if="imgSlider?.images?.length" :images="imgSlider.images" />
 
-      <div
-        v-if="blocks.top || blocks.middle || blocks.bottom"
-        class="h-full flex flex-col justify-between relative pointer-events-none z-[4]"
-      >
+      <div v-if="blocks.top || blocks.middle || blocks.bottom" class="h-full flex flex-col justify-between relative pointer-events-none z-[4]">
         <CardBlock v-bind="blocks.top" />
         <CardBlock v-bind="blocks.middle" />
         <CardBlock v-bind="blocks.bottom" />
@@ -60,18 +57,12 @@ const { lg, md, sm, xs, vh } = defineProps({
       top: undefined,
     }),
   },
-  bgImg: {
+  background: {
     type: Object,
     default: () => ({
       url: "",
       position: "",
       size: "",
-    }),
-  },
-  bgVideo: {
-    type: Object,
-    default: () => ({
-      url: "",
     }),
   },
 })
