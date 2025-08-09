9b7a6e47e5e78d5e245d8b17a14faf7006047e0c
t-poirier <tri.poirier@basikon.io>
2025-08-09 17:48:55 +0200

    404

--- app/pages/404.vue
+++ app/pages/404.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="col-xs-12">404</div>
+  <div class="col-xs-12">
+    <div class="h-[90vh] flex items-center justify-center">{{ $t("pageNotFound") }}</div>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
